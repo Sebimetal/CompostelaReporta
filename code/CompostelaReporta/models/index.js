a9d0150d6cfdaae25e45be3338d446e46c3fdf9c
A22DanielSC <a22danielsc@iessanclemente.net>
2024-06-11 22:36:52 +0200

    Actualizando os documentos

--- code/CompostelaReporta/models/index.js
+++
@@ -1,5 +0,0 @@
-const Issue = require('./issue');
-const Responder = require('./responder');
-const Update = require('./update');
-
-module.exports = { Issue, Responder, Update };
--- code/CompostelaReporta/models/responder.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const responderSchema = new Schema({
-  name: { type: String },
-  telephone: { type: String },
-});
-
-module.exports = mongoose.model('Responder', responderSchema);
